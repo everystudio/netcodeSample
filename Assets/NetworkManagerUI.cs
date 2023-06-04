9c4ad1973b37bd8155a71e989f07b1c8811cff57
watanabe@marida <watanabe.everystudio@gmail.com>
2023-06-05 00:17:59 +0900

    netcode テストプロジェクト

---
+++ Assets/NetworkManagerUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Unity.Netcode;
+
+public class NetworkManagerUI : MonoBehaviour
+{
+    [SerializeField] private Button hostButton;
+    [SerializeField] private Button clientButton;
+
+    private void Awake()
+    {
+        hostButton.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartHost();
+        });
+        clientButton.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartClient();
+        });
+    }
+}
