bc1de18212e6c06f633cbc692d8890c1b1e6dc96
watanabe@marida <watanabe.everystudio@gmail.com>
2024-01-05 09:25:27 +0900

    Matchmaker機能

--- Assets/NetworkManagerUI.cs
+++ Assets/NetworkManagerUI.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 using Unity.Netcode;
 using System;
 
@@ -8,35 +9,126 @@ using System.Threading.Tasks;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
 
+using Unity.Services.Matchmaker;
+using Unity.Services.Matchmaker.Models;
+using StatusOptions = Unity.Services.Matchmaker.Models.MultiplayAssignment.StatusOptions;
+
 public class NetworkManagerUI : MonoBehaviour
 {
-    [SerializeField] private Button hostButton;
     [SerializeField] private Button clientButton;
 
     private void Awake()
     {
-        hostButton.onClick.AddListener(() =>
-        {
-            NetworkManager.Singleton.StartServer();
-        });
+        clientButton.interactable = false;
         clientButton.onClick.AddListener(() =>
         {
-            NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData("34.143.223.103", 9000);
-            NetworkManager.Singleton.StartClient();
+            // マッチングを開始する処理を記載する
+            CreateTicket();
+            clientButton.interactable = false;
         });
     }
-    /*
-    private void Start()
+
+    async private void Start()
     {
-        //SignIn();
+        bool isClient = true;
+        var args = System.Environment.GetCommandLineArgs();
+        for (int i = 0; i < args.Length; i++)
+        {
+            if (args[i] == "-dedicatedServer")
+            {
+                isClient = false;
+            }
+        }
+        if (isClient)
+        {
+            await UnityServices.InitializeAsync();
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            clientButton.interactable = true;
+        }
     }
 
-    private async void SignIn()
+    private async void CreateTicket()
     {
-        await UnityServices.InitializeAsync();
-        await AuthenticationService.Instance.SignInAnonymouslyAsync();
-        clientButton.interactable = true;
-    }
-    */
+        var options = new CreateTicketOptions("Queue-1vs1");
 
+        var players = new List<Player>
+        {
+            new Player(AuthenticationService.Instance.PlayerId,
+                new MatchmakingPlayerData
+                {
+                    skill = 100
+                })
+        };
+
+        var ticketResponse = await MatchmakerService.Instance.CreateTicketAsync(players, options);
+        string ticketID = ticketResponse.Id;
+
+        Debug.Log($"Created ticket {ticketID}");
+
+        PollTicketStatus(ticketID);
+    }
+
+    private async void PollTicketStatus(string ticketID)
+    {
+        MultiplayAssignment multiplayAssignment = null;
+        bool isAssigning = true;
+        bool isFound = false;
+        do
+        {
+            await Task.Delay(1000);
+            var ticketStatus = await MatchmakerService.Instance.GetTicketAsync(ticketID);
+            if (ticketStatus == null)
+            {
+                Debug.Log("Ticket not found");
+                continue;
+            }
+            if (ticketStatus.Type == typeof(MultiplayAssignment))
+            {
+                multiplayAssignment = ticketStatus.Value as MultiplayAssignment;
+
+                switch (multiplayAssignment.Status)
+                {
+                    case StatusOptions.Found:
+                        Debug.Log("Found match");
+                        isAssigning = false;
+                        isFound = true;
+                        TicketAssigned(multiplayAssignment);
+                        break;
+                    case StatusOptions.InProgress:
+                        break;
+                    case StatusOptions.Failed:
+                        isAssigning = false;
+                        Debug.Log($"Failed to get ticket status. Error:{multiplayAssignment.Message}");
+                        break;
+                    case StatusOptions.Timeout:
+                        isAssigning = false;
+                        Debug.Log($"Ticket timed out. Error:{multiplayAssignment.Message}");
+                        break;
+                    default:
+                        throw new InvalidOperationException();
+                }
+
+            }
+
+        }
+        while (isAssigning);
+
+        clientButton.interactable = !isFound;
+    }
+    private void TicketAssigned(MultiplayAssignment multiplayAssignment)
+    {
+        Debug.Log($"Ticket assigned:{multiplayAssignment.Ip}:{multiplayAssignment.Port}");
+        NetworkManager.Singleton.GetComponent<UnityTransport>().
+        SetConnectionData(
+            multiplayAssignment.Ip,
+            (ushort)multiplayAssignment.Port);
+
+        NetworkManager.Singleton.StartClient();
+    }
+
+    [Serializable]
+    public class MatchmakingPlayerData
+    {
+        public int skill;
+    }
 }
--- Assets/ServerStartup.cs
+++ Assets/ServerStartup.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 
+using Unity.Services.Core;
+using System.Threading.Tasks;
+using Unity.Services.Multiplay;
+using Unity.Services.Matchmaker;
+
 public class ServerStartup : MonoBehaviour
 {
     //private string externalConnetionString => $"{externalServerIP}:{serverPort}";
+    private IServerQueryHandler serverQueryHandler;
 
-    private void Start()
+    async private void Start()
     {
         bool isServer = false;
         ushort serverPort = 7777;
@@ -34,12 +40,45 @@ public class ServerStartup : MonoBehaviour
         if (isServer)
         {
             StartServer(externalServerIP, serverPort);
+            await StartServerServices();
         }
     }
 
     private void StartServer(string ip, ushort port)
     {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
+
         NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(ip, port);
         NetworkManager.Singleton.StartServer();
     }
+
+    async Task StartServerServices()
+    {
+        await UnityServices.InitializeAsync();
+        try
+        {
+            // ここはサーバーにアクセス出来るプレイヤーの合計数を指定
+            // 現在は仮として2人を設定してますが、ゲームによって変更する必要あり
+            ushort maxPlayers = 2;
+            serverQueryHandler = await MultiplayService.Instance.StartServerQueryHandlerAsync(maxPlayers, "n/a", "n/a", "0", "n/a");
+        }
+        catch (UnityException e)
+        {
+            Debug.LogError(e.Message);
+        }
+    }
+    private void Update()
+    {
+        if (serverQueryHandler != null)
+        {
+            if (NetworkManager.Singleton.IsServer)
+            {
+                serverQueryHandler.CurrentPlayers = (ushort)NetworkManager.Singleton.ConnectedClientsIds.Count;
+            }
+            serverQueryHandler.UpdateServerCheck();
+        }
+
+    }
+
 }
